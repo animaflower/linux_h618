f81a0159d7eb271dec4ccc19578f0156a83825f9
Liangqi_linux <872930297@qq.com>
2025-07-29 11:17:01 +0800

    先清空

--- sh/tfa.sh
+++
@@ -1,5 +0,0 @@
-#git clone --depth 1 https://github.com/TrustedFirmware-A/trusted-firmware-a.git
-#cd trusted-firmware-a
-make realclean
-make CROSS_COMPILE=aarch64-linux-gnu- PLAT=sun50i_h616
-cd ..
