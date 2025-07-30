a862924c49998c838b4702dd2cd7eb51be5d1732
Liangqi_linux <872930297@qq.com>
2025-07-30 20:11:57 +0800

    从一个盘启动

--- bin/boot.cmd
+++ bin/boot.cmd
@@ -1,4 +1,4 @@
-setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p2 init=/init rw rootwait
+setenv bootargs console=ttyS0,115200 root=/dev/mmcblk0p1 init=/init rw rootwait
 
 fatload mmc 0:1 0x42000000 Image
 fatload mmc 0:1 0x45C00000 sun50i-h618-orangepi-zero2w.dtb
