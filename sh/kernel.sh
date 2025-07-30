bacf03f489d035fae3a0093740bbd5911b846a5c
Liangqi_linux <872930297@qq.com>
2025-07-30 10:15:47 +0800

    17.8M

--- sh/kernel.sh
+++ sh/kernel.sh
@@ -28,11 +28,12 @@ set -e
 # Device Drivers > Graphics support >  Direct Rendering Manager    Serial ATA and Parallel ATA drivers        
 # SCSI device support  Multimedia support   Staging drivers PCI GNSS  Multiple devices driver support  
 # LED Support  Platform support for Chrome hardware  FPGA Configuration Framework    ACPI
-
+# VFIO Non-Privileged      Virtio drivers     VHOST drivers   
 
 # File systems > Dnotify support       Filesystem wide access notification       Quota support    FUSE   EFI Variable filesystem 
-# 
+# Miscellaneous filesystems
 
+# EFI
 
 # sudo make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- tinyconfig
 
