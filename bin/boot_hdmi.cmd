e098960a1fe8034567b10bee8beb458888146186
Liangqi_linux <872930297@qq.com>
2025-07-31 19:32:41 +0800

    hdmi显示

---
+++ bin/boot_hdmi.cmd
@@ -0,0 +1,6 @@
+setenv bootargs console=tty0 console=ttyS0,115200 root=/dev/mmcblk0p1 init=/init rw rootwait
+
+fatload mmc 0:1 0x42000000 Image
+fatload mmc 0:1 0x45C00000 sun50i-h618-orangepi-zero2w.dtb
+
+booti 0x42000000 - 0x45C00000
