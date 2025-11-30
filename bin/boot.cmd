be053f91479cd8ba5bfa1bbfdf411e7397665710
liangqi <872930297@qq.com>
2025-11-30 11:34:04 +0800

    内核加载到官方推荐为止给内核500M空间

--- bin/boot.cmd
+++ bin/boot.cmd
@@ -1,6 +1,6 @@
-setenv bootargs console=ttyS0,115200 earlycon root=/dev/mmcblk0p1 init=/init rw rootwait\n
+setenv bootargs root=/dev/mmcblk0p2 vt.global_cursor_default=0 init=/helloworld rw rootwait
 
-fatload mmc 0:1 0x42000000 Image
-fatload mmc 0:1 0x45C00000 sun50i-h618-orangepi-zero2w.dtb
+load mmc 0:1 0x40080000 Image
+load mmc 0:1 0x50000000 sun50i-h618-orangepi-zero2w.dtb
 
-booti 0x42000000 - 0x45C00000
+booti 0x40080000 - 0x50000000
